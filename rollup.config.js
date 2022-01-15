dfbdc980178c337080858ce5e1c649ce71858979
Michael Jungo <michaeljungo92@gmail.com>
2022-01-15 03:44:06 +0100

    Combine all output bundles into one rollup run
    The reason the cjs bundle was created separately, was due to the fact,
    that for Node, the `TextEncoder` needed to be imported from `util`,
    because at the time it wasn't available globally, so it needed to be
    injected into the bundle.

--- rollup.config.js
+++ rollup.config.js
@@ -5,28 +5,25 @@ import babel from "rollup-plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import replace from "rollup-plugin-replace";
 
-const isNode = process.env.TARGET === "node";
-
 const wasmBytes = Array.from(
   readFileSync(resolve(__dirname, "src/xxhash.wasm"))
 );
 
-const output = isNode
-  ? {
-      file: "cjs/xxhash-wasm.cjs",
-      format: "cjs",
-      sourcemap: true,
-      exports: "default",
-    }
-  : [
-      { file: "esm/xxhash-wasm.js", format: "es", sourcemap: true },
-      {
-        file: "umd/xxhash-wasm.js",
-        format: "umd",
-        name: "xxhash",
-        sourcemap: true,
-      },
-    ];
+const output = [
+  {
+    file: "cjs/xxhash-wasm.cjs",
+    format: "cjs",
+    sourcemap: true,
+    exports: "default",
+  },
+  { file: "esm/xxhash-wasm.js", format: "es", sourcemap: true },
+  {
+    file: "umd/xxhash-wasm.js",
+    format: "umd",
+    name: "xxhash",
+    sourcemap: true,
+  },
+];
 const replacements = {
   WASM_PRECOMPILED_BYTES: JSON.stringify(wasmBytes),
 };
