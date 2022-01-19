f2277acb12030eddc6c16e0afdcc459c1b06b3fc
Sebastian Werner <s.werner@sebastian-software.de>
2022-01-19 16:25:35 +0100

    Export data types separately + fixed bigint data type

--- types.d.ts
+++ types.d.ts
@@ -1,18 +1,19 @@
-declare module "xxhash-wasm" {
-  type Hash<T> = {
-    update(input: string | Uint8Array): Hash<T>;
-    digest(): T;
-  }
+export type XXHash<T> = {
+  update(input: string | Uint8Array): XXHash<T>;
+  digest(): T;
+}
 
-  type Exports = {
-    h32(input: string, seed?: number): number;
-    h32ToString(input: string, seed?: number): string;
-    h32Raw(inputBuffer: Uint8Array, seed?: number): number;
-    create32(seed?: number): Hash<number>;
-    h64(input: string, seed?: BigInt): BigInt;
-    h64ToString(input: string, seed?: BigInt): string;
-    h64Raw(inputBuffer: Uint8Array, seed?: BigInt): BigInt;
-    create64(seed?: BigInt): Hash<BigInt>;
-  };
-  export default function xxhash(): Promise<Exports>;
+export type XXHashAPI = {
+  h32(input: string, seed?: number): number;
+  h32ToString(input: string, seed?: number): string;
+  h32Raw(inputBuffer: Uint8Array, seed?: number): number;
+  create32(seed?: number): XXHash<number>;
+  h64(input: string, seed?: bigint): bigint;
+  h64ToString(input: string, seed?: bigint): string;
+  h64Raw(inputBuffer: Uint8Array, seed?: bigint): bigint;
+  create64(seed?: bigint): XXHash<bigint>;
+};
+
+declare module "xxhash-wasm" {
+  export default function xxhash(): Promise<XXHashAPI>;
 }
